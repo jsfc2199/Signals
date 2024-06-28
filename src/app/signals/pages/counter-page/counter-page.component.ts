decbc63bd92846129af0cd3efffa9377e1c5c19e
Juanse Franco <jsfc2199@gmail.com>
2024-06-28 13:39:48 -0500

    Counter usando señal y señal computada

--- src/app/signals/pages/counter-page/counter-page.component.ts
+++ src/app/signals/pages/counter-page/counter-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 
 @Component({
   selector: 'app-counter-page',
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrl: './counter-page.component.css'
 })
 export class CounterPageComponent {
+
+  public counter = signal(10)
+
+  //señal computada de solo lectura. Queda entonces pendiente de los cambios de otra señal. Es de solo lectura
+  public squareCounter = computed(() => this.counter() * this.counter())
+
+  increaseBy(value: number){
+    // this.counter.set(this.counter() + value)
+
+    //forma "correcta" con signals
+    this.counter.update(current => current + value)
+  }
+
 
 }
