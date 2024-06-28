a2d877c43ca183cdcd28e822c7d80893c517331b
Juanse Franco <jsfc2199@gmail.com>
2024-06-28 14:12:21 -0500

    se consume el servicio y se usan signals para mostrar usuarios

--- src/app/signals/pages/user-info-page/user-info-page.component.ts
+++ src/app/signals/pages/user-info-page/user-info-page.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { UsersServiceService } from '../../services/users-service.service';
+import { User } from '../../interfaces/user.interface';
 
 @Component({
   selector: 'app-user-info-page',
@@ -6,5 +8,26 @@ import { Component } from '@angular/core';
   styleUrl: './user-info-page.component.css'
 })
 export class UserInfoPageComponent {
+
+  private userService = inject(UsersServiceService)
+  public userId = signal(1)
+
+  currentUser = signal<User | undefined>(undefined)
+  userWsFound = signal(true)
+
+  ngOnInit(): void {
+    this.loadUser(this.userId())
+
+  }
+
+  loadUser(id: number){
+    if(id <= 0) return
+    this.userId.set(id)
+
+    this.userService.getUserById(id)
+    .subscribe( user => {
+      this.currentUser.set(user)
+    })
+  }
 
 }
