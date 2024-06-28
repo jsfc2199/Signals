6af4d6bc4f64e7baa63213e536ee0bcd7515013d
Juanse Franco <jsfc2199@gmail.com>
2024-06-28 13:12:36 -0500

    primer acercamiento del uso de signals convirtiendo el side menu en una señal

--- src/app/signals/components/side-menu/side-menu.component.ts
+++ src/app/signals/components/side-menu/side-menu.component.ts
@@ -1,21 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 interface MenuItem {
-  title:string,
-  route: String
+  title: string;
+  route: string;
 }
 @Component({
   selector: 'side-menu',
   templateUrl: './side-menu.component.html',
-  styleUrl: './side-menu.component.css'
+  styleUrl: './side-menu.component.css',
 })
 export class SideMenuComponent {
-
-  //Todo Transformar en una señal
   //!Nota se puede considerar las señales como programación reactiva
-  public menuItems: MenuItem[] = [
-    {title: 'Contador', route: 'counter'},
-    {title: 'Usuario', route: 'user-info'},
-    {title: 'Mutaciones', route: 'properties'},
-  ]
+  // public menuItems: MenuItem[] = [
+  //   {title: 'Contador', route: 'counter'},
+  //   {title: 'Usuario', route: 'user-info'},
+  //   {title: 'Mutaciones', route: 'properties'},
+  // ]
+
+  public menuItems = signal<MenuItem[]>([
+    { title: 'Contador', route: 'counter' },
+    { title: 'Usuario', route: 'user-info' },
+    { title: 'Mutaciones', route: 'properties' },
+  ]);
 }
